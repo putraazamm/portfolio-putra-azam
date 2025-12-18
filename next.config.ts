95136ea0879ca0867b4d8032f11e3e355a88d10e
putraazamm <putraazamm44@gmail.com>
2025-12-18 08:49:07 +0800

    edit a line in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/poftfolio-putra-azam",
+  basePath: "/portfolio-putra-azam",
   images: {
     unoptimized: true,
   },
